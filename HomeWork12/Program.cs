ab4eb92ff31bf7a9081c5689f342e8608290081e
Timothy <Thron41k@gmail.com>
2024-12-15 03:21:29 +0800

    work in progress

--- HomeWork12/Program.cs
+++ HomeWork12/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HomeWork12;
+
+List<User> users =
+[
+    new( "Tom",  "Tom", true),
+    new( "Alice","Alice" ),
+    new( "Bob", "Bob", true ),
+    new( "Kate", "Kate" ),
+    new( "Sam", "Sam", true )
+];
+GreetingUsers(users);
+return;
+
+static void GreetingUsers(List<User> users)
+{
+    foreach (var user in users)
+    {
+        Console.WriteLine($"Приветствую, {user.Name}!");
+        if (user.IsPremium) continue;
+        ShowAds();
+    }
+}
+
+static void ShowAds()
+{
+    Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+    // Остановка на 1 с
+    Thread.Sleep(1000);
+
+    Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+    // Остановка на 2 с
+    Thread.Sleep(2000);
+
+    Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+    // Остановка на 3 с
+    Thread.Sleep(3000);
+}
---
+++ HomeWork12/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork12
+{
+    internal class User(string login, string name, bool isPremium = false)
+    {
+        public string Login { get; set; } = login;
+        public string Name { get; set; } = name;
+        public bool IsPremium { get; set; } = isPremium;
+    }
+}
